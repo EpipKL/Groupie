fe43a98df944a96cfe7d392aba57546c29f9b964
Aaron <attanner86@gmail.com>
2023-03-31 01:29:03 -0600

    tour dates

--- assets/script.js
+++ assets/script.js
@@ -12,9 +12,11 @@ search.addEventListener('click', () => {
         bandName = input.value;
         discography.innerHTML = ''
         bioDiv.innerHTML = '';
+        tour.innerHTML = '';
         discography.textContent = 'Discography';
         getDiscogs();
         getLast();
+        getTicket();
 })
 
 // get album cover images and titles from Discogs API
@@ -66,6 +68,39 @@ function getLast() {
         })
 }
 
+const ticketKey = 'UcycjsGfTYWd0RhGEctxiUWtpRjqQXm8'
+const ticketSecret = 'HWE3OUWKExknLpdY'
+let city = 'Denver';
+const photo = document.querySelector('#photo');
+const tour = document.querySelector('#tour');
+
+let tourDate = [];
+let tourVenue = [];
+let tourCity = [];
+let tourState = [];
+let tourEl = [];
+
+function getTicket() {
+    fetch('https://app.ticketmaster.com/discovery/v2/events.json?keyword='+bandName+'&countryCode=US&sort=date,asc&apikey='+ticketKey)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            for (let i = 0; i < 10; i++) {
+                tourDate[i] = data._embedded.events[i].dates.start.localDate;
+                tourVenue[i] = data._embedded.events[i]._embedded.venues[0].name;
+                tourCity[i] = data._embedded.events[i]._embedded.venues[0].city.name;
+                tourState[i] = data._embedded.events[i]._embedded.venues[0].state.stateCode;
+                tourEl[i] = document.createElement('button');
+                tourEl[i].classList.add('button');
+                tourEl[i].classList.add('is-success');
+                tourEl[i].textContent = tourCity[i] + ', ' + tourState[i] + " | " + tourVenue[i] + " | " + tourDate[i];
+                tour.appendChild(tourEl[i]);
+            }
+        })
+}
+
 // variables and open/close functions for modal
 const discModal = document.querySelector('#disc-modal');
 const discBtn = document.querySelector('#disc-modal-btn');
@@ -77,3 +112,18 @@ discBtn.addEventListener('click', () => {
 discCloseBtn.addEventListener('click', () => {
     discModal.classList.remove('is-active');
 })  
+
+// SEAT GEEK API --- NOT THAT GREAT, SAVE FOR LATER
+// const seatId = 'MTE1Mjc5NzB8MTY4MDIyMzkzNy4wNDg4ODY1'
+// const seatSecret = '748b.......'
+// const querySeat = 'https://api.seatgeek.com/2/performers?q='+bandName+'client_id='+seatId;
+
+// function getSeat() {
+//     fetch('https://api.seatgeek.com/2/performers?slug='+bandName+'&client_id='+seatId)
+//         .then(function(response) {
+//             return response.json();
+//         })
+//         .then(function(data) {
+//             console.log(data);
+//         })
+// }
